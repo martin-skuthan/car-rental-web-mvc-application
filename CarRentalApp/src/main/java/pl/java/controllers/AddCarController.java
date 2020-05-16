6828cac9fa526b82576cd8a82fd6d4260a4e7c63
martin.skuthan <martin.skuthan95@gmail.com>
2020-05-16 21:29:46 +0200

    Feature - Adding cars - Improvements

--- CarRentalApp/src/main/java/pl/java/controllers/AddCarController.java
+++ CarRentalApp/src/main/java/pl/java/controllers/AddCarController.java
@@ -30,54 +30,36 @@ public class AddCarController extends HttpServlet {
 		String typeOfCarFromRequest = request.getParameter("typeOfCar");
 		TypeOfCar typeOfCar = TypeOfCar.getFromDescription(typeOfCarFromRequest);
 		HashMap<CarFields, String> carDetailsFromRequest = null;
-		switch (typeOfCar) {
-		case PASSENGER_CAR:
-			carDetailsFromRequest = getPassengerCarDetails(request);
-			carService.createCar(carDetailsFromRequest, typeOfCar);
-			break;
-		case LIGHT_COMMERCIAL_CAR:
-			carDetailsFromRequest = getLightCommercialCarDetails(request);
-			carService.createCar(carDetailsFromRequest, typeOfCar);
-			break;
-		default:
-			response.sendError(403);
-		}
-		
+		carDetailsFromRequest = getCarDetails(request, typeOfCar);
+		carService.createCar(carDetailsFromRequest, typeOfCar);
 		final String operation = "Adding car";
 		request.setAttribute("operation", operation);
 		request.getRequestDispatcher("/WEB-INF/hidden-views/operation-success.jsp").forward(request, response);
 	}
+	
+	private HashMap<CarFields, String> getCarDetails(HttpServletRequest request, TypeOfCar typeOfCar) {
+		HashMap<CarFields, String> carDetails = new HashMap<CarFields, String>();
+		carDetails.put(CarFields.REGISTRATION_NUMBER, request.getParameter("inputRegistrationNumber"));
+		carDetails.put(CarFields.BRAND, request.getParameter("inputBrand"));
+		carDetails.put(CarFields.MODEL, request.getParameter("inputModel"));
+		carDetails.put(CarFields.SEATS, request.getParameter("inputSeats"));
+		carDetails.put(CarFields.AIR_CONDITIONING, request.getParameter("inputAirConditioning"));
+		carDetails.put(CarFields.TRANSMISSION, request.getParameter("inputTransmission"));
+		switch (typeOfCar) {
+		case PASSENGER_CAR:
+			carDetails.put(CarFields.NUMBER_OF_DOORS, request.getParameter("inputNumberOfDoors"));
+			carDetails.put(CarFields.TYPE_OF_DRIVE, request.getParameter("inputTypeOfDrive"));
+			carDetails.put(CarFields.TRUNK_CAPACITY, request.getParameter("inputTrunkCapacity"));
+			break;
+		case LIGHT_COMMERCIAL_CAR:
+			carDetails.put(CarFields.PAYLOAD, request.getParameter("inputTrunkPayload"));
+			carDetails.put(CarFields.LOAD_VOLUME, request.getParameter("inputLoadVolume"));
+			carDetails.put(CarFields.LOAD_HEIGHT, request.getParameter("inputLoadHeight"));
+			carDetails.put(CarFields.LOAD_WIDTH, request.getParameter("inputLoadWidth"));
+			carDetails.put(CarFields.LOAD_LENGTH, request.getParameter("inputLoadLength"));
+			break;
+		}
 		
-	private HashMap<CarFields, String> getPassengerCarDetails(HttpServletRequest request) {
-		HashMap<CarFields, String> passengerCarDetails = new HashMap<CarFields, String>();
-		passengerCarDetails.put(CarFields.REGISTRATION_NUMBER, request.getParameter("inputRegistrationNumber"));
-		passengerCarDetails.put(CarFields.BRAND, request.getParameter("inputBrand"));
-		passengerCarDetails.put(CarFields.MODEL, request.getParameter("inputModel"));
-		passengerCarDetails.put(CarFields.SEATS, request.getParameter("inputSeats"));
-		passengerCarDetails.put(CarFields.AIR_CONDITIONING, request.getParameter("inputAirConditioning"));
-		passengerCarDetails.put(CarFields.TRANSMISSION, request.getParameter("inputTransmission"));
-		passengerCarDetails.put(CarFields.NUMBER_OF_DOORS, request.getParameter("inputNumberOfDoors"));
-		passengerCarDetails.put(CarFields.TYPE_OF_DRIVE, request.getParameter("inputTypeOfDrive"));
-		passengerCarDetails.put(CarFields.TRUNK_CAPACITY, request.getParameter("inputTrunkCapacity"));
-		return passengerCarDetails;
+		return carDetails;
 	}
-	
-	
-	private HashMap<CarFields, String> getLightCommercialCarDetails(HttpServletRequest request) {
-		HashMap<CarFields, String> passengerCarDetails = new HashMap<CarFields, String>();
-		passengerCarDetails.put(CarFields.REGISTRATION_NUMBER, request.getParameter("inputRegistrationNumber"));
-		passengerCarDetails.put(CarFields.BRAND, request.getParameter("inputBrand"));
-		passengerCarDetails.put(CarFields.MODEL, request.getParameter("inputModel"));
-		passengerCarDetails.put(CarFields.SEATS, request.getParameter("inputSeats"));
-		passengerCarDetails.put(CarFields.AIR_CONDITIONING, request.getParameter("inputAirConditioning"));
-		passengerCarDetails.put(CarFields.TRANSMISSION, request.getParameter("inputTransmission"));
-		passengerCarDetails.put(CarFields.PAYLOAD, request.getParameter("inputTrunkPayload"));
-		passengerCarDetails.put(CarFields.LOAD_VOLUME, request.getParameter("inputLoadVolume"));
-		passengerCarDetails.put(CarFields.LOAD_HEIGHT, request.getParameter("inputLoadHeight"));
-		passengerCarDetails.put(CarFields.LOAD_WIDTH, request.getParameter("inputLoadWidth"));
-		passengerCarDetails.put(CarFields.LOAD_LENGTH, request.getParameter("inputLoadLength"));
-		return passengerCarDetails;
-	}
-	
-
 }
